b0b8fe95f2ac7d7dcd1d0805fa3740b3e2c0b7f7
huiyi <chuahy222@gmail.com>
2023-08-23 15:32:27 +0800

    flutter download file with dio

---
+++ lib/main.dart
@@ -0,0 +1,146 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:open_file/open_file.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'File Download',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'File Download'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  TextEditingController url = TextEditingController();
+  double _progress = 0.0;
+  bool _downloading = false;
+
+  Future openFile({required String url, String? fileName}) async {
+    final name = fileName ?? url.split('/').last;
+    final file = await downloadFile(url, name);
+
+    if (file == null) return;
+
+    print('Path: ${file.path}');
+
+    OpenFile.open(file.path);
+  }
+
+  Future<File?> downloadFile(String url, String name) async {
+    // final appStorage = await getExternalStorageDirectory();
+    // final appStorage = await getTemporaryDirectory();
+    final appStorage = await getApplicationDocumentsDirectory();
+    final file = File('${appStorage.path}/$name');
+
+    try {
+      final response = await Dio().get(url,
+          options: Options(
+            responseType: ResponseType.bytes,
+            followRedirects: false,
+          ), onReceiveProgress: (received, total) {
+        if (total != -1) {
+          setState(() {
+            _progress = received / total;
+          });
+        }
+      });
+
+      final raf = file.openSync(mode: FileMode.write);
+      raf.writeFromSync(response.data);
+      await raf.close();
+
+      return file;
+    } catch (e) {
+      return null;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Download file',
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                controller: url,
+                decoration: const InputDecoration(label: Text('Url')),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            OutlinedButton(
+              onPressed: () async {
+                setState(() {
+                  _downloading = true;
+                  _progress = 0.0;
+                });
+
+                final status = await Permission.storage.request();
+                if (status.isGranted) {
+                  await openFile(url: url.text);
+                } else {
+                  print('Permission denied');
+                }
+
+                setState(() {
+                  _downloading = false;
+                  _progress = 0.0;
+                });
+              },
+              child: Text(
+                'Download',
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
+            if (_downloading)
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: LinearProgressIndicator(
+                  value: _progress,
+                ),
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
